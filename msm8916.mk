fd1d9568ab84f285e18b559052cb0cb905956c5d
Ajay Singh Parmar <aparmar@codeaurora.org>
2015-05-27 14:07:39 -0700

    msm8916: Add CEC support for 8916
    Enable CEC HAL to integrate CEC framework and CEC hardware.
    
    Change-Id: Iaa30d3f67beabf18d5a22e8a5a147b92889dca7b

--- msm8916.mk
+++ msm8916.mk
@@ -94,3 +94,8 @@ PRODUCT_PACKAGES += \
     wpa_supplicant_overlay.conf \
     p2p_supplicant_overlay.conf
 
+#HDMI CEC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.hdmi.cec.xml:system/etc/permissions/android.hardware
+.hdmi.cec.xml
+
